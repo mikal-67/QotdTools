d559a6fef01d31b940d8d4648d6910a8d7e0e5a2
Mikal <72887058+mikal-67@users.noreply.github.com>
2021-02-13 16:50:49 -0600

    Add files via upload

---
+++ src/QotdClient.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+
+namespace QotdClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IPAddress ip;
+            if (args.Length > 0 && IPAddress.TryParse(args[0], out ip))
+            {
+
+            }
+            else
+            {
+                ip = IPAddress.Loopback;
+            }
+
+            try
+            {
+                var client = new Socket(SocketType.Stream, ProtocolType.Tcp);
+                client.Connect(ip, 17);
+                byte[] bytes = new byte[500];
+                client.Receive(bytes);
+                Console.WriteLine(System.Text.Encoding.ASCII.GetString(bytes));
+            }
+            catch (SocketException)
+            {
+                Console.WriteLine("Error: " + (args.Length > 0 ? ip.ToString() : "The current computer") + " is not hosting a Quote of the Day server.");
+            }
+        }
+    }
+}
---
+++ src/QotdServer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+
+namespace QotdServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Socket sock = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            //RFC 865 says the the QOTD should be limited to 500 chars.
+            //This is a check for that.
+            string t = File.ReadAllText("message.txt");
+            string message = t.Length < 500 ? t : "";
+            //Start the listener. If the program is closed, it'll automatically stop.
+            sock.Bind(new IPEndPoint(IPAddress.Any, 17));
+            sock.Listen(100);
+
+            while (true)
+            {
+                
+                var client = sock.Accept();
+                client.Send(System.Text.Encoding.ASCII.GetBytes(message));
+            }
+        }
+    }
+}
